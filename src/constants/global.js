7f8a76a1007c24a4317c97224c3d5d86b523c7aa
Syed Ali Naqi Hasni <engr.syedalinaqihasni@gmail.com>
2025-06-29 18:28:42 +0500

    init

---
+++ src/constants/global.js
@@ -0,0 +1,43 @@
+const mainNavItems = [
+  { name: 'Facts', href: '/facts' },
+  // { name: 'News', href: '/news' },
+  // { name: 'Business', href: '/business' },
+  // { name: 'Mobiles', href: '/mobiles' },
+  // { name: 'Cricket', href: '/cricket' },
+  // { name: 'Videos', href: '/videos' },
+  // { name: 'Autos', href: '/autos' },
+  // { name: 'Health', href: '/health' },
+  // { name: 'Women', href: '/women' },
+  // { name: 'Islam', href: '/islam' },
+  // { name: 'Article', href: '/article' },
+  // { name: 'Names', href: '/names' },
+  // { name: 'Dictionary', href: '/dictionary' },
+];
+
+const moreNavItems = [
+  { name: 'more', href: '/more' },
+  // { name: 'Shop', href: '/shop' },
+  // { name: 'English News', href: '/english-news' },
+  // { name: 'Profiles', href: '/profiles' },
+  // { name: 'Facts', href: '/facts' },
+  // { name: 'Property', href: '/property' },
+  // { name: 'Recipes', href: '/recipes' },
+  // { name: 'Politics', href: '/politics' },
+  // { name: 'Poetry', href: '/poetry' },
+  // { name: 'Quotes', href: '/quotes' },
+  // { name: 'Results', href: '/results' },
+  // { name: 'Calculators', href: '/calculators' },
+  // { name: 'Directory', href: '/directory' },
+  // { name: 'MyPage', href: '/mypage' },
+  // { name: 'Photos', href: '/photos' },
+  // { name: 'Urdu Editor', href: '/urdu-editor' },
+  // { name: 'Travel & Tours', href: '/travel-tours' },
+];
+const footerLinks = [
+  { text: 'About us', url: '/about-us/' },
+  { text: 'Contact us', url: '/contact-us/' },
+  { text: 'Advertising', url: '/advertising/' },
+  { text: 'Privacy Policy', url: '/privacy-policy/' },
+];
+
+export { mainNavItems, moreNavItems, footerLinks };
